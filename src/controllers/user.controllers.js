0c70515d47d73e91a0bfcff07038a6e601f475a8
JeanKingG <137098569+JeanKing03@users.noreply.github.com>
2024-09-21 14:28:48 -0400

    Api_CrudUser Finalizada

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,48 @@
+const catchError = require("../utils/catchError");
+const user = require("../models/user");
+const { use } = require("express/lib/application");
+const { joinSQLFragments } = require("sequelize/lib/utils/join-sql-fragments");
+const { json } = require("sequelize");
+
+const create = catchError(async (req, res) => {
+  const result = await user.create(req.body);
+  return res.status(201).json(result);
+});
+
+const getAll = catchError(async (req, res) => {
+  const result = await user.findAll();
+  return res.status(200).json(result);
+});
+
+const getOne = catchError(async (req, res) => {
+  const { id } = req.params;
+  const result = await user.findByPk(id);
+  if (!result) return res.status(404).json({ message: "User Not Found!" });
+  return res.status(200).json(result);
+});
+
+const update = catchError(async (req, res) => {
+  const { id } = req.params;
+  const result = await user.update(req.body, {
+    where: { id },
+    returning: true,
+  });
+  if (result[0] === 0)
+    return res.status(404).json({ message: "User Not Found!" });
+  return res.status(200).json(result);
+});
+
+const destroy = catchError(async (req, res) => {
+  const { id } = req.params;
+  const result = await user.destroy({ where: { id } });
+  if (!result) return res.status(404).json({ message: "User Not Found!" });
+  return res.status(200).json({ message: "Deleted User!" });
+});
+
+module.exports = {
+  create,
+  getOne,
+  getAll,
+  update,
+  destroy,
+};
---
+++ src/models/user.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/connection");
+const { password } = require("pg/lib/defaults");
+
+const user = sequelize.define("user", {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  birthDay: {
+    allowNull: false,
+    type: DataTypes.DATEONLY,
+  },
+});
+
+module.exports = user;
